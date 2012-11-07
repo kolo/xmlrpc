421ef8ea7beb680a614dfdb2e3a90889a4b534ef
Mike McClurg <mike.mcclurg@gmail.com>
2012-11-07 21:58:34 +0000

    Unit test for untyped responses are strings
    Signed-off-by: Mike McClurg <mike.mcclurg@gmail.com>

--- response_test.go
+++ response_test.go
@@ -1,8 +1,8 @@
 package xmlrpc
 
 import (
-    "testing"
-    "time"
+	"testing"
+	"time"
 )
 
 const RESPONSE_BODY = `
@@ -41,19 +41,51 @@ const FAULT_RESPONSE_BODY = `
 </methodResponse>`
 
 func Test_parseResponseBody_SuccessfulResponse(t *testing.T) {
-    result, err := parseResponse([]byte(RESPONSE_BODY))
+	result, err := parseResponse([]byte(RESPONSE_BODY))
 
-    if err != nil {
-        t.Fatalf("parseResponse raised error: %v", err)
-    }
+	if err != nil {
+		t.Fatalf("parseResponse raised error: %v", err)
+	}
 
-    time, _ := time.Parse("2006012T15:04:05", "20120911T18:16:01")
-    assert_equal(t, time, result)
+	time, _ := time.Parse("2006012T15:04:05", "20120911T18:16:01")
+	assert_equal(t, time, result)
 }
 
 func Test_parseResponseBody_FaultResponse(t *testing.T) {
-    _, err := parseResponse([]byte(FAULT_RESPONSE_BODY))
-    assert_not_nil(t, err)
+	_, err := parseResponse([]byte(FAULT_RESPONSE_BODY))
+	assert_not_nil(t, err)
 }
 
+const XENAPI_RESPONSE = `
+<methodResponse>
+  <params>
+    <param>
+      <value>
+        <struct>
+          <member>
+            <name>Status</name>
+            <value>Success</value>
+          </member>
+          <member>
+            <name>Value</name>
+            <value>OpaqueRef:4b40767e-bc91-ca34-7e11-0ca46bb6b3e0</value>
+          </member>
+        </struct>
+      </value>
+    </param>
+  </params>
+</methodResponse>
+`
 
+func Test_parse_XenAPI_ResponseBody(t *testing.T) {
+	result, err := parseResponse([]byte(XENAPI_RESPONSE))
+
+	expected := map[string]string{
+		"Value":  "OpaqueRef:4b40767e-bc91-ca34-7e11-0ca46bb6b3e0",
+		"Status": "Success",
+	}
+
+	assert_nil(t, err)
+	assert_equal(t, result.(Struct)["Value"], expected["Value"])
+	assert_equal(t, result.(Struct)["Status"], expected["Status"])
+}
